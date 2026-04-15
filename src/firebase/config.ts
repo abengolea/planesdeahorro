a8332710126aeaae5cbc02f8b6cda03c5c97b159
Adrian Bengolea <adrianbengolea@notificas.com>
2026-04-15 20:10:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6685306101-ef56b",
+  "appId": "1:122016303331:web:2ba6cdc55b5dba33735dd6",
+  "apiKey": "AIzaSyAWRYYLg2nZaClRqLSnGH16Ap-dH2i330U",
+  "authDomain": "studio-6685306101-ef56b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "122016303331"
+};
